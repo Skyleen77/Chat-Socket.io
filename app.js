b9413b62a87e4f6eb3f5435fc9d4facd17df4178
Skyleen77 <elsk8@outlook.fr>
2022-04-02 23:06:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+// Modules
+let express = require('express');
+let http = require('http');
+let socketio = require('socket.io');
+let morgan = require('morgan');
+let config = require('./config');
+
+// Variables globales
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+const port = config.express.port;
+const options = {
+  root: __dirname + '/views'
+}
+
+// Middlewares
+app.use(express.static(options.root))
+app.use(morgan('dev'));
+
+// Routes
+app.get('/', (req, res) => {
+  res.redirect('/home');
+});
+
+app.get('/home', (req, res) => {
+  console.log();
+  res.sendFile('index.html', options);
+});
+
+app.get('/params/:name', (req, res) => {
+  res.send(req.params.name);
+});
+
+// IO
+io.on('connection', socket => {
+  console.log('user connected');
+});
+
+// Lancement de l'application
+server.listen(port, () => {
+  console.log(`Serveur started on port ${port}`);
+});
